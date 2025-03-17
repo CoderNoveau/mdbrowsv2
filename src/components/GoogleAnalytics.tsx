b555e6ae6df9c1966628d810b769242f4c64e549
CoderNoveau <thomastpnguyen@gmail.com>
2025-03-18 10:40:51 +1100

    updating all changes to master

--- src/components/GoogleAnalytics.tsx
+++ src/components/GoogleAnalytics.tsx
@@ -2,26 +2,35 @@
 
 import Script from 'next/script';
 
+// Declare GA ID as a constant
+const GA_ID = 'G-Y2NNP8B3YY';
+
 const GoogleAnalytics = () => {
   return (
-    <>
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=G-Y2NNP8B3YY`}
-      />
-      <Script
-        id="google-analytics"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
+    <Script
+      id="google-analytics"
+      strategy="lazyOnload"
+    >
+      {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+
+        if (typeof window !== 'undefined') {
+          const script = document.createElement('script');
+          script.src = 'https://www.googletagmanager.com/gtag/js?id=${GA_ID}';
+          script.async = true;
+          document.head.appendChild(script);
+
+          script.onload = function() {
             gtag('js', new Date());
-            gtag('config', 'G-Y2NNP8B3YY');
-          `,
-        }}
-      />
-    </>
+            gtag('config', '${GA_ID}', {
+              send_page_view: true,
+              debug_mode: ${process.env.NODE_ENV === 'development'}
+            });
+          };
+        }
+      `}
+    </Script>
   );
 };
 
