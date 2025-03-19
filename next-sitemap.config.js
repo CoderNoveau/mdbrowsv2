b8ca9376a72dce07ffc1819057f6e5ad76d6ac84
CoderNoveau <thomastpnguyen@gmail.com>
2025-03-20 01:23:39 +1100

    Update sitemap configuration to use non-www URL consistently

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://www.mdbrows.com.au',
+  siteUrl: 'https://mdbrows.com.au',
   generateRobotsTxt: true,
   robotsTxtOptions: {
     policies: [
@@ -12,15 +12,4 @@ module.exports = {
   },
   exclude: ['/server-sitemap.xml'],
   sitemapSize: 5000,
-  transform: (config, url) => {
-    // Ensure all URLs use www
-    const modifiedUrl = url.replace('https://mdbrows.com.au', 'https://www.mdbrows.com.au')
-    return {
-      loc: modifiedUrl,
-      changefreq: config.changefreq,
-      priority: config.priority,
-      lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
-      alternateRefs: config.alternateRefs ?? [],
-    }
-  }
-} 
+}; 
